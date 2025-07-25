f36934c3eaa6ed49c73570a7051cc609e2a7d0cc
raidtahri <220400166+raidtahri@users.noreply.github.com>
2025-07-25 23:21:41 +0100

    fix: pass IMAGE_NAME and IMAGE_TAG from Jenkinsfile to shared library

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,5 +1,5 @@
 #!usr/bin/env groovy
-def call() {
+def call(String IMAGE_NAME, String IMAGE_TAG) {
           dir('06-hello-maven-shared-library-pipeline') {
                 withCredentials([usernamePassword(
                     credentialsId: 'dockerhub',
--- vars/pushToDockerHub.groovy
+++ vars/pushToDockerHub.groovy
@@ -1,5 +1,5 @@
 #!usr/bin/env groovy
-def call() {
+def call(String IMAGE_NAME, String IMAGE_TAG) {
 
 withCredentials([usernamePassword(
                     credentialsId: 'dockerhub',
