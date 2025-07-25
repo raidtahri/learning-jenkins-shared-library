665dbf997c0d97c17d4583209a51428325ba7086
raidtahri <220400166+raidtahri@users.noreply.github.com>
2025-07-26 00:43:37 +0100

    fix(.groovy): correctly set parameters in buildDockerImage.groovy and pushToDockerHub.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,5 +1,5 @@
 #!usr/bin/env groovy
-def call(String IMAGE_NAME, String IMAGE_TAG) {
+def call(imagaName, imageTag) {
           dir('06-hello-maven-shared-library-pipeline') {
                 withCredentials([usernamePassword(
                     credentialsId: 'dockerhub',
--- vars/pushToDockerHub.groovy
+++ vars/pushToDockerHub.groovy
@@ -1,5 +1,5 @@
 #!usr/bin/env groovy
-def call(String imageName, String imageTag) {
+def call(imageName, imageTag) {
 
 withCredentials([usernamePassword(
                     credentialsId: 'dockerhub',
